8431690efdba3fa891637cde25c317e9b305e67f
syifawhn <syifawahyuni@mhs.mdp.ac.id>
2022-11-30 17:06:20 +0700

    done

---
+++ app/src/main/java/com/si5a/dailyquote/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.si5a.dailyquote.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.si5a.dailyquote.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
